9381247fef5d6ff4ce10c826c31dafaf95a8808d
“[Vivek <[imvvk7@gmail.com]>
2020-02-17 11:57:00 +0530

    till adding a file

---
+++ heap_sort.cpp
@@ -0,0 +1,170 @@
+/*Implement and compare the following sorting algorithm Radix Sort
+Also compare these algorithm with the previous algorithm(quick sort, merge sort)*/
+/*RadixSort QuickSort MergeSort*/
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(int arr[], int l, int m, int r) {
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+
+	int L[n1], R[n2];
+
+	for (i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (j = 0; j < n2; j++)
+		R[j] = arr[m + 1+ j];
+
+	i = 0;
+	j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			arr[k] = L[i];
+			i++;
+		}
+		else{
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	while (i < n1) {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	while (j < n2) {
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+}
+
+void mergeSort(int arr[], int l, int r) {
+	if (l < r) {
+		int m = l+(r-l)/2;
+		mergeSort(arr, l, m);
+		mergeSort(arr, m+1, r);
+		merge(arr, l, m, r);
+	}
+}
+
+
+int flagquick=0;
+
+void countSort(int arr[], int n, int pos){
+    int count[10];
+    int output[n];
+    for(int i = 0; i <= 9; i++){
+        count[i] = 0;
+    }
+    for(int i = 0 ; i < n ; i++){
+        count[(arr[i]/pos)%10]++;
+    }
+
+
+    for(int i = 1; i <= 9; i++){
+        count[i] = count[i]+ count[i-1];
+    }
+
+    for(int i = n-1; i >=0; i--){
+        output[count[ (arr[i]/pos)%10 ] - 1] = arr[i];
+        count[ (arr[i]/pos)%10 ]--;
+    }
+
+    for(int i = 0; i < n; i++)
+    arr[i]  = output[i];
+}
+
+void radixSort(int arr[], int n){
+    int m = arr[0];
+    for(int i = 1; i < n; i++){
+        if(arr[i] > m){
+            m = arr[i];
+        }
+    }
+}
+
+
+int partition (int arr[], int l, int h) {
+    int pivot = arr[h];
+    int i = (l - 1);
+
+    for (int j = l; j <= h - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+        flagquick+=1;
+    }
+    swap(arr[i + 1], arr[h]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int l, int h) {
+    if (l < h) {
+        int pi = partition(arr, l, h);
+
+        quickSort(arr, l, pi - 1);
+        quickSort(arr, pi + 1, h);
+    }
+}
+
+void writeItB(int n, double t) {
+   fstream fout;
+    fout.open("report1.csv", ios::out | ios::app);
+        // Insert the data to file
+        fout << n << ", " << t << "\n";
+}
+
+void writeItII(int n, double t) {
+   fstream fout;
+    fout.open("report2.csv", ios::out | ios::app);
+        // Insert the data to file
+        fout << n << ", " << t << "\n";
+}
+
+void writeItSS(int n, double t) {
+   fstream fout;
+    fout.open("report3.csv", ios::out | ios::app);
+        // Insert the data to file
+        fout << n << ", " << t << "\n";
+}
+
+int main() {
+   for(int n=100;n<=100000;n+=5000){
+    int arr1[n];
+    int arr2[n];
+    int arr3[n];
+    for(int i=0;i<n;i++) {
+        int temp=rand();
+        arr2[i]=temp;
+        arr1[i]=temp;
+        arr3[i]=temp;
+    }
+    clock_t start, end;
+    start = clock();
+    quickSort(arr1, 0, n-1);
+    end = clock();
+    double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+    writeItII(n, time_taken);
+
+    start = clock();
+    mergeSort(arr2, 0, n-1);
+    end = clock();
+    time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+    writeItSS(n, time_taken);
+
+    start = clock();
+    radixSort(arr3, n);
+    end = clock();
+    time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+    writeItB(n, time_taken);
+  }
+
+    return 0;
+}
---
+++ main.py
@@ -0,0 +1,132 @@
+from docx import Document
+from docx.shared import Inches, Cm
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.shared import Pt
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+# Some Inputs
+ip_file_name = "heap_sort.cpp"
+op_file_name = 'Algo_LAB_AS.docx'
+
+# compiling and running c++ file
+os.system("g++ " + ip_file_name)
+os.system("./a.out")
+colorV = ('red', 'blue', 'green')
+
+
+def plot_graph(ip):
+    count = 0
+    for i in ip:
+        count += 1
+        report_var = 'report' + str(count) + '.csv'
+        report = pd.read_csv(report_var)
+        plt.plot(report.iloc[:, 0].values, report.iloc[:, 1].values)
+    # report1 = pd.read_csv('report1.csv')
+    # report2 = pd.read_csv('report2.csv')
+    # report3 = pd.read_csv('report3.csv')
+    # plt.plot(report1.iloc[:, 0].values, report1.iloc[:, 1].values, color='red')
+    # plt.plot(report2.iloc[:, 0].values, report2.iloc[:, 1].values, color='blue')
+    # plt.plot(report3.iloc[:, 0].values, report3.iloc[:, 1].values, color='green')
+
+    title_v = ''
+    if len(ip) > 1:
+        title_v = 'comparing '
+        for i in ip:
+            if i == ip[0]:
+                title_v = title_v + ' ' + i
+            else:
+                title_v = title_v + ', ' + i
+    else:
+        title_v = ip[0]
+
+    plt.title(title_v)
+    plt.legend(ip)
+    plt.xlabel('n')
+    plt.ylabel('time taken')
+    plt.savefig('fig.png')
+
+
+# Creating document object
+document = Document()
+
+sections = document.sections
+section = sections[0]
+section.top_margin = Inches(0.5)
+
+lab_no = 1
+
+# Heading like LAB1
+paragraph = document.add_paragraph()
+# run = paragraph.add_run('LAB {}'.format(lab_no))
+run = paragraph.add_run(ip_file_name[:-4])
+font = run.font
+paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
+font.size = Pt(22)
+font.name = 'Arial'
+font.bold = True
+# paragraph = document.add_paragraph('LAB1')
+# paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
+# paragraph.style = document.styles['Heading 1']
+
+
+# Aim: by aim var
+paragraph = document.add_paragraph()
+run = paragraph.add_run('Aim: ')
+run.font.bold = True
+run.font.name = 'sans serif'
+
+# Reading from cpp file
+f = open('heap_sort.cpp', 'r')
+
+aim = ''
+for line in f:
+    if line[0:2] == '/*':
+        aim = aim + line[2:-1] + ' '
+    elif line[-3:-1] == '*/':
+        aim = aim + line[:-3]
+        f.close()
+        break
+    else:
+        aim = aim + line[:-1] + ' '
+run = paragraph.add_run(aim)
+run.font.name = 'sans serif'
+
+# Codes
+f = open('heap_sort.cpp', 'r')
+codes = ''
+k = True
+
+for line in f:
+    if k:
+        if line[-3:-1] == '*/':
+            k = False
+    elif k==False and line[-3:-1] == '*/':
+        ips = line[2:-3].split()
+    else:
+        codes = codes + line
+f.close()
+paragraph = document.add_paragraph()
+run = paragraph.add_run('Codes: \n')
+run.font.bold = True
+run.font.name = 'sans serif'
+run = paragraph.add_run(codes)
+run.font.name = 'sans serif'
+document.add_page_break()
+
+# Output Section
+paragraph = document.add_paragraph()
+run = paragraph.add_run('Output: ')
+run.font.bold = True
+run.font.name = 'sans serif'
+
+# Adding graph to docs
+plot_graph(ips)
+document.add_picture('fig.png', width=Cm(15), height=Cm(11.66))
+document.add_page_break()
+
+document.save(op_file_name)
+
+for i in range(len(ips)):
+    os.remove('report'+str(i+1)+'.csv')
